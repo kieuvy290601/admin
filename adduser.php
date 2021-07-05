f3af2cc16fb031a8db3147bd33b55cfb35587e07
kieuvy290601 <vyntkgcd191163@fpt.edu.vn>
2021-07-05 18:46:04 +0700

    echiuroii

--- adduser.php
+++ adduser.php
@@ -1,45 +1,26 @@
-<?php
-$host_heroku = "ec2-52-86-25-51.compute-1.amazonaws.com";
-$db_heroku = "dcnnn5f87r5pst";
-$user_heroku = "nlcmibxyahfytv";
-$pw_heroku =
-"df1ff1c44e44f986fd52f17f3da8346a956efc0d65553c3b187d88514683d65e";
-$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-$pg_heroku = pg_connect($conn_string);
-if (!$pg_heroku)
-{
-die('Error: Could not connect: ' . pg_last_error());
-}
-?>
-<?php
 
+ <?php
 session_start();
 include("../db.php");
 include "sidenav.php";
 include "topheader.php";
+if(isset($_POST['btn_save']))
+{
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$email=$_POST['email'];
+$user_password=$_POST['password'];
+$mobile=$_POST['phone'];
+$address1=$_POST['city'];
+$address2=$_POST['country'];
 
-if($_GET['btn_save'])
-{
-$first_name=$_GET['first_name'];
-$last_name=$_GET['last_name'];
-$email=$_GET['email'];
-$user_password=$_GET['password'];
-$mobile=$_GET['phone'];
-$address1=$_GET['city'];
-$address2=$_GET['country'];
-$query = "INSERT INTO user_info VALUES ('$user_id', '$first_name', '$last_name', '$email', '$password', '$mobile', '$address1', '$address2')";
-$data = pg_query($pg_heroku,$query);
-if($data)
-{
-echo "<script>alert('Added Successfully!')</script>";
-?>
-<?php
+mysqli_query($con,"insert into user_info(first_name, last_name,email,password,mobile,address1,address2) values ('$first_name','$last_name','$email','$user_password','$mobile','$address1','$address2')") 
+			or die ("Query 1 is inncorrect........");
+header("location: manage_users.php"); 
+mysqli_close($con);
 }
-else
-{
-echo "Failed to update the table.";
-}
-}
+
+
 ?>
       <!-- End Navbar -->
       <div class="content">
@@ -52,21 +33,19 @@ echo "Failed to update the table.";
                   <p class="card-category">Complete User profile</p>
                 </div>
                 <div class="card-body">
-                  <form action="" method="GET">
+                  <form action="" method="post" name="form" enctype="multipart/form-data">
                     <div class="row">
                       
-
-
                       <div class="col-md-3">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">First Name</label>
-                          <input type="text" value="" name="first_name" class="form-control" required>
+                          <input type="text" id="first_name" name="first_name" class="form-control" required>
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">Last Name</label>
-                          <input type="text"  value="" name="last_name" class="form-control" required>
+                          <input type="text" name="last_name" id="last_name"  class="form-control" required>
                         </div>
                       </div>
                     </div>
@@ -74,13 +53,13 @@ echo "Failed to update the table.";
                       <div class="col-md-6">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">Email</label>
-                          <input type="email"  value="" name="email"  class="form-control" required>
+                          <input type="email" name="email" id="email" class="form-control" required>
                         </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">Password</label>
-                          <input type="password"  value="" name="password" class="form-control" required>
+                          <input type="password" id="password" name="password" class="form-control" required>
                         </div>
                       </div>
                     </div>
@@ -88,7 +67,7 @@ echo "Failed to update the table.";
                       <div class="col-md-12">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">phone number</label>
-                          <input type="text" value="" name="phone" class="form-control" required>
+                          <input type="text" id="phone" name="phone" class="form-control" required>
                         </div>
                       </div>
                     </div>
@@ -96,19 +75,19 @@ echo "Failed to update the table.";
                       <div class="col-md-4">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">City</label>
-                          <input type="text" name="city" value=""  class="form-control" required>
+                          <input type="text" name="city" id="city"  class="form-control" required>
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">Address</label>
-                          <input type="text" name="country" value="" class="form-control" required>
+                          <input type="text" name="country" id="country" class="form-control" required>
                         </div>
                       </div>
                       
                     </div>
                     
-                    <button type="submit" name="btn_save" value="" class="btn btn-primary pull-right">Update User</button>
+                    <button type="submit" name="btn_save" id="btn_save" class="btn btn-primary pull-right">Update User</button>
                     <div class="clearfix"></div>
                   </form>
                 </div>
