1515968eba6b61879f0470e2308379a8554b7e0b
kieuvy290601 <vyntkgcd191163@fpt.edu.vn>
2021-07-05 14:35:05 +0700

    2 commit

---
+++ edituser.php
@@ -0,0 +1,93 @@
+
+  <?php
+session_start();
+include("../db.php");
+$user_id=$_REQUEST['user_id'];
+
+$result=mysqli_query($con,"select user_id,first_name,last_name, email, password from user_info where user_id='$user_id'")or die ("query 1 incorrect.......");
+
+list($user_id,$first_name,$last_name,$email,$user_password)=mysqli_fetch_array($result);
+
+if(isset($_POST['btn_save'])) 
+{
+
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$email=$_POST['email'];
+$user_password=$_POST['password'];
+
+mysqli_query($con,"update user_info set first_name='$first_name', last_name='$last_name', email='$email', password='$user_password' where user_id='$user_id'")or die("Query 2 is inncorrect..........");
+
+header("location: manageuser.php");
+mysqli_close($con);
+}
+include "sidenav.php";
+include "topheader.php";
+?>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid">
+        <div class="col-md-5 mx-auto">
+            <div class="card">
+              <div class="card-header card-header-primary">
+                <h5 class="title">Edit User</h5>
+              </div>
+              <form action="edituser.php" name="form" method="post" enctype="multipart/form-data">
+              <div class="card-body">
+                
+                  <input type="hidden" name="user_id" id="user_id" value="<?php echo $user_id;?>" />
+                    <div class="col-md-12 ">
+                      <div class="form-group">
+                        <label>First name</label>
+                        <input type="text" id="first_name" name="first_name"  class="form-control" value="<?php echo $first_name; ?>" >
+                      </div>
+                    </div>
+                    <div class="col-md-12 ">
+                      <div class="form-group">
+                        <label>Last name</label>
+                        <input type="text" id="last_name" name="last_name" class="form-control" value="<?php echo $last_name; ?>" >
+                      </div>
+                    </div>
+                    <div class="col-md-12 ">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email"  id="email" name="email" class="form-control" value="<?php echo $email; ?>">
+                      </div>
+                    </div>
+                    <div class="col-md-12 ">
+                      <div class="form-group">
+                        <label >Password</label>
+                        <input type="text" name="password" id="password" class="form-control" value="<?php echo $user_password; ?>">
+                      </div>
+                    </div>
+                  
+                  
+                  
+                
+              </div>
+              <div class="card-footer">
+                <button type="submit" id="btn_save" name="btn_save" class="btn btn-fill btn-primary">Update</button>
+              </div>
+              </form>    
+            </div>
+          </div>
+         
+          
+        </div>
+      </div>
+      <?php
+include "footer.php";
+?>
+<?php
+$host_heroku = "ec2-52-86-25-51.compute-1.amazonaws.com";
+$db_heroku = "dcnnn5f87r5pst";
+$user_heroku = "nlcmibxyahfytv";
+$pw_heroku =
+"df1ff1c44e44f986fd52f17f3da8346a956efc0d65553c3b187d88514683d65e";
+$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+$pg_heroku = pg_connect($conn_string);
+if (!$pg_heroku)
+{
+die('Error: Could not connect: ' . pg_last_error());
+}
+?>
---
+++ sumit_form.php
@@ -0,0 +1,44 @@
+<?php
+$host_heroku = "ec2-52-86-25-51.compute-1.amazonaws.com";
+$db_heroku = "dcnnn5f87r5pst";
+$user_heroku = "nlcmibxyahfytv";
+$pw_heroku =
+"df1ff1c44e44f986fd52f17f3da8346a956efc0d65553c3b187d88514683d65e";
+$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+$pg_heroku = pg_connect($conn_string);
+if (!$pg_heroku)
+{
+die('Error: Could not connect: ' . pg_last_error());
+}
+?>
+<?php
+$link=$_REQUEST['success'];
+?>
+<html>
+<head>
+<script>
+function myfunction()
+{
+	document.getElementById("success").click();
+}
+</script>
+</head>
+<body onLoad="myfunction()">
+<form action="index.php" method="post" id="myform">
+	<input hidden="" type="text" name="success" value="<?php echo"$link";?>">
+    <button hidden="" type="submit" id="success" data-modal="success" name="success" value="success"></button>
+</form>
+
+<h1 style="padding-left:400px; padding-top:200px;"><img src="../images/loading-x.gif" /></h1>
+</body>
+</html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
